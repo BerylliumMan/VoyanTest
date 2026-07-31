4d1369a5606e153d89ec6e0c46c054b10cb0c67f
BerylliumMan <41931153@qq.com>
2026-07-31 10:51:46 +0800

    feat: 执行侧语义保真提示词与 browser-use 日志回传
    强化 execution/operation_translate 与 browser-use 步骤任务的语义保真约束，并经 RUN_LOG WebSocket 将客户端执行日志回传到服务端。

---
+++ frontend/src/pages/testcases/functional/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TestCases from '../index';
+
+const FunctionalTestCasesPage: React.FC = () => (
+  <TestCases caseKind="functional" />
+);
+
+export default FunctionalTestCasesPage;
